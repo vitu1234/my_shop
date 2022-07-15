de2d069151e051b1d50b24bcb63e476ecbff863f
Vitu12345 <vmafeni@agilesystems.pa.mw>
2022-07-16 00:34:10 +0200

    working on flash products and all products page

---
+++ pages/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'native-base';
+
+function Products(props) {
+    return (
+        <View>
+            <Text>
+                All Products
+            </Text>
+        </View>
+    );
+}
+
+export default Products;
