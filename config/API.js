2683a930258998f9fe1ccc39fafd54c1d2acccca
Vt <mafeniblessed@gmail.com>
2022-09-18 01:47:58 +0900

    - added alert to the app - post request added to the register button

--- config/API.js
+++ config/API.js
@@ -94,7 +94,7 @@ const getProductsScreen = async (props) => {
         .catch((err) => {
           props.productsScreenLoading(true, err.message);
         });
-    }else if (props.category_id === -1) {
+    } else if (props.category_id === -1) {
       fetch(`${base_url}/product`, {
         method: "GET", // default, so we can ignore
       })
@@ -175,4 +175,34 @@ const getProductsScreen = async (props) => {
 
 };
 
-export { base_url, getProductsScreen, getHomeScreen, base_urlImages };
+//register user account
+const registerUserAccount = async (data) => {
+  // Example POST method implementation:
+
+  // Default options are marked with *
+  const response = await fetch(`${base_url}/user`, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  }).then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // return response.json(); // parses JSON response into native JavaScript objects
+
+
+};
+
+export { base_url, getProductsScreen, getHomeScreen, registerUserAccount, base_urlImages };
